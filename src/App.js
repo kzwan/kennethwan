02ef5924bc70648758e068bc013fb6f3ec9e4600
Kenny Wan <kennethzwan@gmail.com>
2023-11-05 01:27:26 -0700

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Typing from "./components/typing";
+import CircleText from "./components/circleText";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div className="App">
+        <Typing/>
+        {/* <CircleText /> */}
+      </div>
+      <div style={{ position: 'absolute', marginTop: '50vh' }}>
+        <CircleText/>
+      </div>
+      <p>hello</p>
     </div>
   );
 }
+
 
 export default App;
